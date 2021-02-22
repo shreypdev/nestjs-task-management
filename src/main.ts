e93f7349e105c02d65528ea16ae533af7b43aaae
Shrey Patel <60997794+shreypdev@users.noreply.github.com>
2021-02-22 17:26:31 +0530

    Pre-production changes

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,12 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV === 'development')
+  if (process.env.NODE_ENV === 'development') {
     app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requets from origin "${serverConfig.origin}"`);
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
